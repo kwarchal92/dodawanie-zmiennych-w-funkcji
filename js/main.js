2cbecca1e10d36deb9d7495faf5c2a1f91b3ee97
kwarchal <kamilwarchal295@gmail.com>
2020-07-21 17:47:12 +0200

    funkcja zwracajaca wynik dodawania

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let firstNumber = 42;
+let secondNumber = 79;
+
+
+function dodawanie(firstNumber, secondNumber) {
+    let suma = firstNumber + secondNumber;
+    return suma;
+ }
+
+let wynik = dodawanie(firstNumber, secondNumber);
+
+console.log(wynik);
+
+
